7c2b8443a5122bacaa3a824aa977c2597bf3fccc
Asher <asherkite@gmail.com>
2025-05-21 00:33:09 +0200

    2.8 and 3.5 python files!

---
+++ Asher JuPyter/QuadraticProgram/prob2-8_May20.py
@@ -0,0 +1,325 @@
+#==============================================================
+# Prob 2.8: Inequality Constrained Quadratic Program Interior Point Solver
+#==============================================================
+#Imports
+import numpy as np
+import scipy as sp
+import casadi as ca
+from casadi import *
+import matplotlib.pyplot as plt
+import time
+
+#==============================================================
+# Part 1: Preparing Our Data
+#==============================================================
+# Import from the file:
+QP_test = sp.io.loadmat('QP_Test.mat')
+#we import with the float datatype to prevent numerical problems further down the line
+QPt_C = (QP_test['C']).astype(float)
+QPt_H = (QP_test['H']).astype(float)
+QPt_dl = (QP_test['dl']).astype(float)
+QPt_du = (QP_test['du']).astype(float)
+QPt_l = (QP_test['l']).astype(float)
+QPt_u = (QP_test['u']).astype(float)
+QPt_g =(QP_test['g']).astype(float)
+QPt_n, _ = QPt_H.shape
+
+#==============================================================
+# The problem we were given was in the form:
+# 
+#      o-------------------------------------o
+#      | min (0.5 * x.T @ H @ x) + (g.T @ x) |
+#      |  x                                  |
+#      | s.t.     dl <= C.T @ x <= du        |
+#      |              l <= x <= u            |
+#      o-------------------------------------o
+#
+# However, we need to convert this to standard form:
+#
+#      o-------------------------------------o
+#      | min (0.5 * x.T @ H @ x) + (g.T @ x) |
+#      |  x                                  |
+#      | s.t.       C_bar.T @ x >= d_bar     |   
+#      o-------------------------------------o
+#
+# We do this refomrulating our dl, du, l, and u bounds into d_bar.
+# Note that:
+#                dl <= C.T @ x <= du
+# Is the same as:
+#                     C.T @ x  >=  dl
+#                   -(C.T @ x) >= -du
+# Similarly:
+#                    l <= x <= u  
+# Is the same as:
+#                       I @ x  >=  l
+#                     -(I @ x) >= -u 
+# This means we want:
+# 
+#         C_bar = | C |   d_bar = | dl|
+#                 |-C |           |-du|
+#                 | I |           | l |
+#                 |-I |           |-u |
+#            
+# We the define the following function to convert to standard form:
+def standard_NoA_IP(C, bl, bu, l, u): #converted to the form expected by interior point
+    n = C.shape[0]
+    Cbar = np.hstack([
+        C,
+        -C,
+        np.eye(n),
+        -np.eye(n)
+    ])
+    dbar = np.concatenate([
+        bl,
+        -bu,
+        l,
+        -u
+    ])
+    return Cbar, dbar
+
+#==============================================================
+# Part 2: The Solver
+#==============================================================
+# We make an inequality constrained quadratic program interior point solver for problems of the form:
+#
+#      o-------------------------------------o
+#      | min (0.5 * x.T @ H @ x) + (g.T @ x) |
+#      |  x                                  |
+#      | s.t.           C.T @ x >= d         |   
+#      o-------------------------------------o
+#
+def InequalityQPSolver(H,g,C,d,x_init= "NoEntry",z_init="NoEntry",s_init="NoEntry",eps = 0.1): #in QP form using C^T * x >= d
+    t = time.process_time_ns()
+    # First we define some functions which will come in handly later:
+    def pos_test(z): #checks if all elements in an array are strictly positive
+        if np.all(z > 0):
+            return True
+        else:
+            return False
+        
+    def NoA_Build_r_val(H,g,C,d,x,z,s,C_T= "NoEntry",print_test = False): #calculate residuals
+        if isinstance(C_T,str):
+            C_T = C.T
+        rL = ( H @ x ) + g  - (C @ z)
+        rC = s + d - (C_T @ x)
+        rsz = np.multiply(z,s)
+        mu = np.mean(rsz)   
+        return rL, rC, rsz, mu 
+    
+    def NoA_Opt_Check(H,g,C,d,x,z,s,eps,mu0,C_T="NoEntry",rL="NoEntry",rC="NoEntry",rsz="NoEntry",mu="NoEntry"):
+        #Taking transposes if they weren't provided
+        if isinstance(C_T,str):
+            C_T = C.T
+        #Calculated r values and mu if they weren't provided
+        if isinstance(rL,str) or isinstance(rC,str) or isinstance(rsz,str) or isinstance(mu,str):
+            rL, rC, rsz, mu = NoA_Build_r_val(H,g,C,d,x,z,s,C_T)
+        #Actual calcuation
+        goodness = 0
+        if np.linalg.norm(rL,ord=np.inf) <= eps * max(1, np.linalg.norm(np.hstack((H,g,C)),ord=np.inf)):
+            goodness += 1
+        dn, dm = d.shape
+        if np.linalg.norm(rC,ord=np.inf) <= eps * max(1, np.linalg.norm(np.hstack((np.eye(dn),d, C_T)),ord=np.inf)):
+            goodness += 1
+        if mu <= eps * (1/100) * mu0:
+            goodness += 1
+        if goodness >=3: return True
+        else: return False
+        
+    def find_a_alt(z,dz,s,ds,x,mult=1): #finds alpha
+        zs_stack = -np.concatenate([z, s], axis=0)
+        dzds_stack = np.concatenate([dz,ds],axis=0)
+        dmask = dzds_stack < 0
+        if len(dzds_stack[dmask]) == 0:
+            a = 1
+        else:
+            a_vec = zs_stack[dmask]/dzds_stack[dmask]
+            a = min(a_vec)
+            #if a > 1:
+            #    a = 1
+            if a <= 0:
+                raise ValueError("###---a nonpositive---###")
+        a *= mult
+        return a
+    #grabbing dimensions
+    Hn, Hm = H.shape
+    Cn, Cm = C.shape
+    #initialize x, z, and s
+    if isinstance(x_init,str):
+        # x_init = np.ones((Hn,1))*10
+        x_init = np.zeros((Hn,1))
+    if isinstance(z_init,str):
+        z_init = np.ones((Cm,1))
+    if isinstance(s_init,str):
+        s_init = np.ones((Cm,1))    
+    #setting variables datatype to float. In tessting we experienced datatype conflicts without this for providing initial x, z, and s values
+    x = x_init.astype(float)
+    z = z_init.astype(float)
+    s = s_init.astype(float)
+
+    #Check for consistent dimensions
+    if not np.all(z > 0):
+        raise ValueError("###---z must be positive---###")
+    if not np.all(s > 0):
+        raise  ValueError("###---s must be positive---###")
+    if Hn != len(x):
+        raise ValueError("###---H and x dimensions do not match---###")
+    if Hm != len(g):
+        raise ValueError("###---H and g dimensions do not match---###")
+    if Cm != len(z):
+        raise ValueError("###---C and z dimensions do not match---###")
+    #Collect Transposes, this means they don't need to be repeatedly calculated. Note that C is never reassigned, so these should be good forever
+    C_T = C.T
+    #Get r and mu values
+    rL, rC, rsz, mu = NoA_Build_r_val(H,g,C,d,x,z,s,C_T) #this function does what it says on the tin: it makes the rL, rA, rC, rsz (and mu) values following the documentation
+    mu0=mu #this grabs the first mu for use in the optimality conditions
+    # Prepare the residuals array
+    ResArray = []
+    ResArray.append([np.linalg.norm(rL,ord=2),np.linalg.norm(rC,ord=2),mu])
+    #start the iteration counter
+    itcount=0
+    #Check for optimality
+    while NoA_Opt_Check(H,g,C,d,x,z,s,eps,mu0,C_T,rL,rC,rsz,mu) != True: #this loop runs so long as the optimality conditions are not met
+        itcount+=1
+        #guardrails
+        if any(np.clip(z,1e-18,1e18) != z):
+            print("z clipped\n",min(z),max(z))
+        z_div = np.clip(z,1e-18,1e18)
+        if any(np.clip(s,1e-18,1e18) != s):
+            print("s clipped\n",min(s),max(s))
+        s_div = np.clip(s,1e-18,1e18) 
+         #this is not strictly in the instructions: I was getting division by 0 errors since z and s were getting really small 
+        if not np.all(z >= 0):
+            raise ValueError("###---while loop error: z must be positive---###")
+        if not np.all(s >= 0):
+            raise ValueError("###---while loop error: s must be positive---###")
+        SinvZ = np.diag((z / s_div).flatten()) #this calculates S^-1 * Z, which is a diagonal matrix.
+        ZinvS = np.diag((s / z_div).flatten()) #calculating Z^-1 * S
+        Zinv  = np.diag((1 / z_div).flatten()) #Z^-1
+        #==============================================================
+        # KKT Matrix and Factorization:
+        #Note that since z and s are technically 2D, I need to flatten z/s before I can plug them into np.diag
+        H_bar = H + (C @ SinvZ @ C_T)
+        #Next I make the KKT matrix
+        KKT = H_bar 
+        L, D, perm = sp.linalg.ldl(KKT) #this is the LDL' decomposition of the KKT matrix
+        L_T = L.T
+        #==============================================================
+        # Affine Step:
+        #Next step is to calculate the affine direction
+        #this next thing is a little weird: (SinvZ @ (rC - Zinv@rsz)) appears several times in the equations, so I just make it a variable
+        #old:               (SinvZ @ (rC - (Zinv@rsz)))
+        #(rC - (Zinv@rsz)) = s + d - C_T@x - Z_invSZe = s + d - C_T@x - Se = s - s + d - C_T@x = d - (C_T@x)
+        reference_block_1 = (SinvZ @ (d - (C_T@x)))
+        rL_bar = rL - ( C @ reference_block_1 ) #calculating rL_bar
+        #here we solve the system using the LDL factorization
+        midd1 = np.linalg.solve(L, -rL_bar)
+        midd2 = np.linalg.solve(D,midd1)
+        xaff = np.linalg.solve(L_T, midd2) 
+        zaff = -(SinvZ @ C_T @ xaff) + reference_block_1
+        saff = - s - d + (C_T @ (xaff + x))
+        #need to get alpha affine
+        aaff = find_a_alt(z,zaff,s,saff,x) #Actually used this step
+        #print("A affine used:",aaff) #testing
+        #next is the duality gap and centering parameter
+        ztest = z + (aaff * zaff)
+        stest = s + (aaff * saff)
+        muaff = np.mean(np.multiply(ztest,stest)) # note
+        #guardrails for numerical stability
+        #note that original definitial is sigma = (muaff / mu)**3
+        sigma = np.clip(min(1, (muaff / mu)**3),0.001,0.999)
+        #==============================================================
+        # Center-Correction step:
+        #affine-centering-correction direction
+        rsz_bar = rsz + np.multiply(saff,zaff) - (sigma * muaff * np.ones((len(rsz),1)))
+        rL_bar = rL - ( C @ SinvZ @ ( rC - ( Zinv @ rsz_bar ) ) )
+        midd1 = np.linalg.solve(L, -rL_bar)
+        midd2 = np.linalg.solve(D,midd1)
+        dx = np.linalg.solve(L_T, midd2) 
+        dz = -(SinvZ @ C_T @ dx) + (SinvZ @ (rC - (Zinv@rsz_bar)))
+        ds = - (Zinv @ rsz_bar) - (-(C_T @ dx) + (rC - (Zinv@rsz_bar)))
+        #calculate a
+        a = find_a_alt(z,dz,s,ds,x)
+        nu = 0.995
+        a_bar = nu * a
+        x += a_bar * dx
+        z += a_bar * dz
+        s += a_bar * ds
+        rL, rC, rsz, mu = NoA_Build_r_val(H,g,C,d,x,z,s,C_T)
+        ResArray.append([np.linalg.norm(rL,ord=2),np.linalg.norm(rC,ord=2),mu]) 
+    cpu_time = time.process_time_ns() - t  
+    return x, z, s, itcount, ResArray, cpu_time
+
+#==============================================================
+# Part 3: Baseline CasADi Solution
+#==============================================================
+# Preparing the variables
+H = QPt_H
+g = QPt_g
+A = QPt_C
+lbx_ = QPt_l
+ubx_ = QPt_u
+lbg_ = QPt_dl
+ubg_ = QPt_du
+nh = QPt_n
+# Define x as a symbolic variable
+x = ca.SX.sym('x', nh) 
+# min (0.5 * x.T @ H @ x) + (g.T @ x)
+objective = 0.5 * ca.dot(x, ca.mtimes(H, x)) + ca.dot(g, x)
+# Constraint: 
+constraint = ca.mtimes(A.T, x)
+# Define the QP in symbolic form
+qp = {
+    'x': x,      
+    'f': objective,
+    'g': constraint
+}
+# Create solver
+solver = ca.qpsol('S', 'qpoases', qp)
+# Call solver with bounds
+sol = solver(
+    lbx=lbx_, 
+    ubx=ubx_,  
+    lbg=lbg_,  
+    ubg=ubg_  
+)
+# getting the actual solution object
+Cas_X_Opt = sol['x']
+
+print(sol['x'])
+
+#==============================================================
+# Part 4: Using Our Solver and Statistics
+#==============================================================
+# preparing our variables
+H = QPt_H
+g = QPt_g
+C, d = standard_NoA_IP(QPt_C,QPt_dl,QPt_du,QPt_l,QPt_u) #using our converer
+#using our function
+xout, zout, sout, iter, res, cpu_time = InequalityQPSolver(H,g,C,d,eps=10e-10)
+print("x:\n",xout.T)
+# Compare the Euclidean distance between CasADi's solution and ours:
+print("Euclidean Distance from Solution:",np.linalg.norm(np.array(Cas_X_Opt).flatten()-xout.flatten(),ord=2))
+
+res = np.array(res)
+res_names = ["rL", "rC", "mu"]
+plt.figure(figsize=(10, 6))
+
+for i in range(res.shape[1]):
+    plt.plot(res[:, i], label=res_names[i])
+
+plt.xlabel('Iteration Number')
+plt.ylabel('Residual L2 Norm')
+plt.title('InequalityQPSolver: L2 Norm of Residuals By Iteration')
+plt.yscale('log') 
+plt.legend()
+plt.grid(True)
+plt.tight_layout()
+#plt.savefig("2-8_Residuals.png")
+plt.show()
+
+print("Number of Iterations:", iter)
+print("CPU Time in seconds:", cpu_time / 1000000000)
+
+#==============================================================
+# End 2.8
+#==============================================================
---
+++ prob3-5.py
@@ -0,0 +1,345 @@
+#==============================================================
+# Problem 3.5: Linear Program Interior Point Solver
+#==============================================================
+# imports
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+import time
+#==============================================================
+# Part 1: Preparing inputs
+#==============================================================
+#Shape printing codes: 1 = print shapes of variables, 0 = no print
+import_print   = 0
+lu_print       = 0
+standard_print = 0
+#==============================================================
+# Import
+LP_test = sp.io.loadmat('./sources/LP_test.mat')
+# Convert to float
+C = LP_test['C'].astype(float)
+Pd_max = LP_test['Pd_max'].astype(float)
+Pg_max = LP_test['Pg_max'].astype(float)
+U = LP_test['U'].astype(float)
+# Check shapes
+if import_print == 1:
+    print("        Shapes:")
+    print("     C:",C.shape)
+    print("Pd_max:",Pd_max.shape)
+    print("Pg_max:",Pg_max.shape)
+    print("     U:",U.shape)
+#==============================================================
+# We first want to get the problem into the form as described in problem 3:
+#
+#         o --------------------- o
+#         |   min   g.T @ x       |
+#         |    x                  |
+#         |   s.t.  A.T @ x  = b  | 
+#         |          l <= x <= u  |
+#         o --------------------- o
+#
+#    x = |pd|    with x.shape = (45,1)
+#        |pg|    pd and pg are variables, not fixed Pd and Pg
+# 
+#    g = |-U|      so g.shape = (45,1)
+#        | C| 
+#
+#    A = | e|    with A.shape = (45,1)
+#        |-e|    Where "e" is a vector of 1s with shape(Pd)
+#                and "-e" is a vector of -1s with shape(Pg)
+#
+#    b = 0         so b.shape = ( 1,1)
+#
+#    l = | 0|    with l.shape = (45,1)
+#
+#    u = |Pd|      so u.shape = (45,1)
+#        |Pg|    here Pd and Pg are the imported arrays
+#
+#==============================================================
+# Build the matrices into the form
+g = np.vstack((-U,C))
+A = np.vstack((np.ones_like(Pd_max),-np.ones_like(Pg_max)))
+b = np.array([[0]])
+l = np.zeros_like(g)
+u = np.vstack((Pd_max,Pg_max))
+# Check Shapes
+if lu_print == 1:
+    print("        Shapes:")
+    print("     g:",g.shape)
+    print("     A:",A.shape)
+    print("     b:",b.shape)
+    print("     l:",l.shape)
+    print("     u:",u.shape)
+#==============================================================
+# Next we want to convert the problem to fit into the solver described in lecture 07B
+#
+#         o --------------------- o
+#         |   min   g.T @ x       |
+#         |    x                  |
+#         |   s.t.  A.T @ x  = b  | 
+#         |               x >= 0  |
+#         o --------------------- o
+#
+# Because of the structure of this problem, the lower bound is actually already 0, so we have:
+#
+#               0 <= x <= u
+#
+# We can then define:
+#
+#              z = |x-0| = | x |     
+#                  |u-x|   |u-x|                                  
+#
+# We want to maintain A.T @ x = b, so with z we have:
+#
+#                    |A|   @ z = |A.T O.T| @ z = b        with O.shape = A.shape
+#                    |O|.T                             where O is a matrix of 0s
+#
+# If we say that the vector x has N elements, we have then that z has 2N elements.
+# For each n in range(N), we then have:
+#
+#                    z_{n} + z_{n+N} = x_{n} + u_{n} - x_{n} = u_{n}
+#
+# These are then N new equality constraints. We add these to our constraints matrix:
+#
+#       A_bar.T = |A.T       O.T       | so we have: A_bar.T @ z = |b|
+#                 |1 0 0 ... 1 0 0 ... |                           |u|  
+#                 |0 1 0 ... 0 1 0 ... |      
+#                 | ...       ...      |       This means: A_bar = |A I|
+#                 | ... 0 1 0 ... 0 1 0|                           |O I|
+#                 | ... 0 0 1 ... 0 0 1|       With I.shape = (len(u),len(u)) = (n,n)   
+#
+# Since we have changed variables, we then also have to change our g. We want to minimize g.T @ x. 
+# Luckily for us, z contains all of x as elements. This means we can just have:
+#
+#         g_bar = |g| so we have: g_bar.T @ z = g.T @ x    with o.shape = (u-x).shape = x.shape
+#                 |o|                                                 where o is a vector of 0s
+# 
+# This then gives us:
+#
+#         o ------------------------- o
+#         |   min   g_bar.T @ z       |
+#         |    z                  |b| |
+#         |   s.t.  A_bar.T @ z = |u| |
+#         |                  z >= 0   |
+#         o ------------------------- o 
+#
+#==============================================================
+#Setting up our variables: 
+un, _ = u.shape
+z     = np.vstack((  np.ones_like(g),
+                   u-np.ones_like(g))) #note here that g.shape = x.shape
+A_bar = np.block([[       A        ,np.eye(un)],  #A_bar = |A I| A_bar.T = |A.T O.T|
+                  [np.zeros_like(A),np.eye(un)]]) #        |O I|           | I   I |
+b_bar = np.vstack((b,
+                   u))
+g_bar = np.vstack((g,
+                   np.zeros_like(g)))
+#Checking shapes:
+if standard_print == 1:
+    print("        Shapes:")
+    print(" g_bar:",g_bar.shape)
+    print(" A_bar:",A_bar.shape)
+    print(" b_bar:",b_bar.shape)
+    print("     z:",z.shape)
+
+#==============================================================
+# Part 2: Solver
+#==============================================================
+# Here we make a linear program interior point solver
+def LPippd(g,A,b,x="Empty"):
+    #==============================================================
+    #The aim is to solve:
+    #
+    #       o--------------------o--------------------o
+    #       | min   g.T @ x                  Lagrange |
+    #       |  x                         Multipliers: |
+    #       | s.t.  A.T @ x  = b          mu, or "mu" |
+    #       |             x >= 0      lambda, or "la" |
+    #       o--------------------o--------------------o
+    #
+    # This solver is based off of linear program interior partial dual solver made by
+    # John Bagterp Joergensen in the file "LPippd.m" offered in Module 5 Lecutre 07B
+    # combined with the pseudocode offered on page 19/22 of the Module 5 Lecture 07B
+    # document "LinearOptimizationIP" on DTU Learn. Link below:
+    # https://learn.inside.dtu.dk/d2l/le/lessons/242131/topics/973897
+    #==============================================================
+    #Initialize Variables:
+    #==============================================================
+    t = time.process_time_ns()
+    maxit = 100
+    tolL = 1.0e-9
+    tolA = 1.0e-9
+    tols = 1.0e-9
+    eta = 0.99
+    n, m = A.shape
+    if type(x) == str: #in case x is not provided, we initialize as ones.
+        x = np.ones_like(g)
+    la = np.ones( (n,1)) #la is lambda
+    mu = np.zeros((m,1))
+    #==============================================================
+    # Next we define necessary functions:
+    #==============================================================
+    #Define a function for computing residuals:
+    def Residuals(g,A,b,x,la,mu):
+        n, m = A.shape
+        rL = g - (A @ mu) - la
+        rA = (A.T @ x) - b
+        rC = np.multiply(x,la)
+        s = np.sum(rC)/n
+        return rL, rA, rC, s
+    #Define a function for checking for convergence:
+    def Convergence(rL, rA, s,tolL=1.0e-9,tolA=1.0e-9,tols=1.0e-9):
+        if np.linalg.norm(rL,ord=np.inf) <= tolL and np.linalg.norm(rA,ord=np.inf) <= tolA and np.abs(s) <= tols:
+            return True
+        else: return False
+    #Function for finding alpha and beta
+    def FindAlpha(z,dz,mult=1): #find smallest a s.t. z + a * dz >= 0
+        dmask = dz < 0
+        if len(dz[dmask]) == 0:
+            a = 1
+        else:
+            a_vec = -z[dmask]/dz[dmask]
+            a = min(a_vec)
+            if a <= 0:
+                raise ValueError("###---a nonpositive---###")
+        a *= mult
+        return a
+    #==============================================================
+    # Next is the while loop construction:
+    #==============================================================
+    rL, rA, rC, s = Residuals(g,A,b,x,la,mu)
+    ResidualsArray = []
+    ResidualsArray.append([np.linalg.norm(rL,ord=2),np.linalg.norm(rA,ord=2),np.linalg.norm(rC,ord=2),s]) #we are asked to report on the residuals as a function of iteration
+    it_count = 0
+    while it_count < maxit and not Convergence(rL,rA,s,tolL,tolA,tols):
+        it_count+=1
+
+        #==============================================================
+        # Make the Hessian matrix and take its Cholesky decomposition:
+        #==============================================================
+        x_lainv = np.divide(x,la)
+        H = A.T @ np.diag(x_lainv.flatten()) @ A
+        L = sp.linalg.cholesky(H, lower=True) #Here we use a scipy function to perform the Cholesky decomposition
+
+        #==============================================================
+        # Affine step calculation + Centering parameter
+        #==============================================================
+        # Prepare the affine right hand side
+        temp_rhs_pre = np.multiply(x,rL)+rC
+        temp_rhs = np.divide(temp_rhs_pre,la)
+        rhs = -rA + (A.T @ temp_rhs)
+        # Next we solve H @ dmu = L @ L.T @ dmu = rhs
+        midd = sp.linalg.solve_triangular(L,rhs,lower=True) 
+        dmu = sp.linalg.solve_triangular(L.T,midd,lower=False)
+        # Then calculate dx and dlambda
+        dx  = np.multiply(x_lainv,A @ dmu) - temp_rhs
+        temp_dla = - (rC + np.multiply(la,dx))
+        dla = np.divide(temp_dla,x)
+        alpha = FindAlpha(x,dx)
+        beta  = FindAlpha(la,dla)
+        # Then we get the affine steps
+        xaff = x + (alpha * dx)
+        laaff = la + (beta * dla)
+        saff = np.sum(np.multiply(xaff,laaff))/n
+        # And centering parameter
+        sigma = (saff/s)**3
+
+        #==============================================================
+        # Centering step calculation
+        #==============================================================
+        # Preparing the centering right hand side
+        rC_bar = rC + np.multiply(dx,dla) - (sigma*s)
+        temp_rhs_pre = np.multiply(x,rL) + rC_bar
+        temp_rhs = np.divide(temp_rhs_pre,la)
+        rhs = -rA + (A.T@temp_rhs)
+        # Next we solve H @ dmu = L @ L.T @ dmu = rhs
+        midd = sp.linalg.solve_triangular(L,rhs,lower=True) 
+        dmu = sp.linalg.solve_triangular(L.T,midd,lower=False)
+        # And calculate dc and dlambda
+        dx = np.multiply(x_lainv,A @ dmu) - temp_rhs
+        temp_dla = - (rC_bar + np.multiply(la,dx))
+        dla = np.divide(temp_dla,x)
+        alpha = FindAlpha(x,dx)
+        beta  = FindAlpha(la,dla)
+        #Then we take the centering step
+        x  = x  + (eta * alpha * dx)
+        mu = mu + (eta * beta * dmu)
+        la = la + (eta * beta * dla)
+
+        #==============================================================
+        # Recompute residuals
+        #==============================================================
+        rL, rA, rC, s = Residuals(g,A,b,x,la,mu)
+        ResidualsArray.append([np.linalg.norm(rL,ord=2),np.linalg.norm(rA,ord=2),np.linalg.norm(rC,ord=2),s]) #keep track of residuals
+    if it_count == maxit:
+        raise ValueError("####---Too Many Iterations---##")
+    cpu_time = time.process_time_ns() - t
+    return x, mu, la, ResidualsArray, it_count, cpu_time
+#==============================================================
+# Part 3: Using the Solver and Statistics
+#==============================================================
+# Solution Baseline From 3.3:
+# 3.3 Used scipy.optimize.linprog, an example of how this can be done is below.
+bounds = []
+ln, _ = l.shape
+for i in range(ln):
+    bounds.append((float(l.flatten()[i]), float(u.flatten()[i])))
+sol = sp.optimize.linprog(c=g.flatten(),A_eq=A.T,b_eq=b.flatten(),bounds=bounds,method='highs')
+sp_x = np.array(sol.x).T 
+# This produces the following solution:
+x_33 = np.array([[225.0], [234.0], [213.0], [228.0], [231.0],
+                 [  0.0], [218.0], [  0.0], [  0.0], [225.0],
+                 [209.0], [  0.0], [222.0], [242.0], [  0.0],
+                 [245.0], [  0.0], [217.0], [  0.0], [242.0],
+                 [243.0], [230.0], [204.0], [230.0], [  0.0],
+                 [  0.0], [245.0], [  0.0], [230.0], [203.0],
+                 [406.0], [  0.0], [560.0], [310.0], [460.0],
+                 [ 45.0], [165.0], [  0.0], [420.0], [  0.0],
+                 [785.0], [  0.0], [  0.0], [410.0], [975.0]])
+#We then confirm that scipy solution matches the one we use in 3.3
+#print("Euclidean Distance Between Official 3.3 Solution and Recalculated SciPy Soltuion",np.linalg.norm(sp_x.flatten()-x_33.flatten(),ord=2))
+#Printing the real solution and cost
+print("Real Solution:\n", x_33.T) # Solution is 2D
+real_cost = (g.T @ x_33)[0,0]     # Use [0,0] to extract the cost value
+print("3.3 Cost:",real_cost)
+
+#==============================================================
+# Our Solution:
+#Using our LPippd
+zout, mu, la, res, iter, cpu_time = LPippd(g_bar,A_bar,b_bar,z) 
+zn, _ = zout.shape
+#Prining our solution and cost
+xout = np.array(zout[0:int(zn/2)]) # remember that our x is the first 45 elements of z. This is a 2 dimensional array
+print("Our Solutiuon:\n", xout.T)  # This means that xout is 2D as well
+our_cost = (g.T @ xout)[0,0]       # We then need to use [0,0] to get our cost
+print("Our Cost:",our_cost)
+
+#==============================================================
+# Comparison:
+print("Solution Distance:", np.linalg.norm((x_33).flatten()-xout.flatten(),ord=2)) # We found a different minima
+print("Cost Difference:",real_cost - our_cost)                                      # But the cost is almost identical, off by an order of e-8
+#==============================================================
+# Plotting residuals:
+# Here we plot the residuals as a funciton of iteration:
+res = np.array(res)
+res_names = ["Lagrangian Gradient", "Equality Constaint", "Complementarity", "Duality Gap"]
+plt.figure(figsize=(10, 6))
+
+for i in range(res.shape[1]):
+    plt.plot(res[:, i], label=res_names[i])
+
+plt.xlabel('Iteration Number')
+plt.ylabel('Residual L2 Norm')
+plt.title('LPippd: L2 Norm of Residuals By Iteration')
+plt.yscale('log') 
+plt.legend()
+plt.grid(True)
+plt.tight_layout()
+plt.show()
+
+print("Number of Iterations:", iter)
+print("CPU Time in seconds:", cpu_time / 1000000000)
+
+#==============================================================
+# End Problem 3.5
+#==============================================================
