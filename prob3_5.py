90e8d32587898f90e2df2d9f48a6b83d20061de6
Asher <asherkite@gmail.com>
2025-05-21 23:31:53 +0200

    Added bonus 3.8

--- prob3_5.py
+++ prob3_5.py
@@ -9,138 +9,139 @@ import time
 #==============================================================
 # Part 1: Preparing inputs
 #==============================================================
-#Shape printing codes: 1 = print shapes of variables, 0 = no print
-import_print   = 0
-lu_print       = 0
-standard_print = 0
-#==============================================================
-# Import
-LP_test = sp.io.loadmat('./sources/LP_test.mat')
-# Convert to float
-C = LP_test['C'].astype(float)
-Pd_max = LP_test['Pd_max'].astype(float)
-Pg_max = LP_test['Pg_max'].astype(float)
-U = LP_test['U'].astype(float)
-# Check shapes
-if import_print == 1:
-    print("        Shapes:")
-    print("     C:",C.shape)
-    print("Pd_max:",Pd_max.shape)
-    print("Pg_max:",Pg_max.shape)
-    print("     U:",U.shape)
-#==============================================================
-# We first want to get the problem into the form as described in problem 3:
-#
-#         o --------------------- o
-#         |   min   g.T @ x       |
-#         |    x                  |
-#         |   s.t.  A.T @ x  = b  | 
-#         |          l <= x <= u  |
-#         o --------------------- o
-#
-#    x = |pd|    with x.shape = (45,1)
-#        |pg|    pd and pg are variables, not fixed Pd and Pg
-# 
-#    g = |-U|      so g.shape = (45,1)
-#        | C| 
-#
-#    A = | e|    with A.shape = (45,1)
-#        |-e|    Where "e" is a vector of 1s with shape(Pd)
-#                and "-e" is a vector of -1s with shape(Pg)
-#
-#    b = 0         so b.shape = ( 1,1)
-#
-#    l = | 0|    with l.shape = (45,1)
-#
-#    u = |Pd|      so u.shape = (45,1)
-#        |Pg|    here Pd and Pg are the imported arrays
-#
-#==============================================================
-# Build the matrices into the form
-g = np.vstack((-U,C))
-A = np.vstack((np.ones_like(Pd_max),-np.ones_like(Pg_max)))
-b = np.array([[0]])
-l = np.zeros_like(g)
-u = np.vstack((Pd_max,Pg_max))
-# Check Shapes
-if lu_print == 1:
-    print("        Shapes:")
-    print("     g:",g.shape)
-    print("     A:",A.shape)
-    print("     b:",b.shape)
-    print("     l:",l.shape)
-    print("     u:",u.shape)
-#==============================================================
-# Next we want to convert the problem to fit into the solver described in lecture 07B
-#
-#         o --------------------- o
-#         |   min   g.T @ x       |
-#         |    x                  |
-#         |   s.t.  A.T @ x  = b  | 
-#         |               x >= 0  |
-#         o --------------------- o
-#
-# Because of the structure of this problem, the lower bound is actually already 0, so we have:
-#
-#               0 <= x <= u
-#
-# We can then define:
-#
-#              z = |x-0| = | x |     
-#                  |u-x|   |u-x|                                  
-#
-# We want to maintain A.T @ x = b, so with z we have:
-#
-#                    |A|   @ z = |A.T O.T| @ z = b        with O.shape = A.shape
-#                    |O|.T                             where O is a matrix of 0s
-#
-# If we say that the vector x has N elements, we have then that z has 2N elements.
-# For each n in range(N), we then have:
-#
-#                    z_{n} + z_{n+N} = x_{n} + u_{n} - x_{n} = u_{n}
-#
-# These are then N new equality constraints. We add these to our constraints matrix:
-#
-#       A_bar.T = |A.T       O.T       | so we have: A_bar.T @ z = |b|
-#                 |1 0 0 ... 1 0 0 ... |                           |u|  
-#                 |0 1 0 ... 0 1 0 ... |      
-#                 | ...       ...      |       This means: A_bar = |A I|
-#                 | ... 0 1 0 ... 0 1 0|                           |O I|
-#                 | ... 0 0 1 ... 0 0 1|       With I.shape = (len(u),len(u)) = (n,n)   
-#
-# Since we have changed variables, we then also have to change our g. We want to minimize g.T @ x. 
-# Luckily for us, z contains all of x as elements. This means we can just have:
-#
-#         g_bar = |g| so we have: g_bar.T @ z = g.T @ x    with o.shape = (u-x).shape = x.shape
-#                 |o|                                                 where o is a vector of 0s
-# 
-# This then gives us:
-#
-#         o ------------------------- o
-#         |   min   g_bar.T @ z       |
-#         |    z                  |b| |
-#         |   s.t.  A_bar.T @ z = |u| |
-#         |                  z >= 0   |
-#         o ------------------------- o 
-#
-#==============================================================
-#Setting up our variables: 
-un, _ = u.shape
-z     = np.vstack((  np.ones_like(g),
-                   u-np.ones_like(g))) #note here that g.shape = x.shape
-A_bar = np.block([[       A        ,np.eye(un)],  #A_bar = |A I| A_bar.T = |A.T O.T|
-                  [np.zeros_like(A),np.eye(un)]]) #        |O I|           | I   I |
-b_bar = np.vstack((b,
-                   u))
-g_bar = np.vstack((g,
-                   np.zeros_like(g)))
-#Checking shapes:
-if standard_print == 1:
-    print("        Shapes:")
-    print(" g_bar:",g_bar.shape)
-    print(" A_bar:",A_bar.shape)
-    print(" b_bar:",b_bar.shape)
-    print("     z:",z.shape)
+if __name__ == "__main__":
+    #Shape printing codes: 1 = print shapes of variables, 0 = no print
+    import_print   = 0
+    lu_print       = 0
+    standard_print = 0
+    #==============================================================
+    # Import
+    LP_test = sp.io.loadmat('./sources/LP_test.mat')
+    # Convert to float
+    C = LP_test['C'].astype(float)
+    Pd_max = LP_test['Pd_max'].astype(float)
+    Pg_max = LP_test['Pg_max'].astype(float)
+    U = LP_test['U'].astype(float)
+    # Check shapes
+    if import_print == 1:
+        print("        Shapes:")
+        print("     C:",C.shape)
+        print("Pd_max:",Pd_max.shape)
+        print("Pg_max:",Pg_max.shape)
+        print("     U:",U.shape)
+    #==============================================================
+    # We first want to get the problem into the form as described in problem 3:
+    #
+    #         o --------------------- o
+    #         |   min   g.T @ x       |
+    #         |    x                  |
+    #         |   s.t.  A.T @ x  = b  | 
+    #         |          l <= x <= u  |
+    #         o --------------------- o
+    #
+    #    x = |pd|    with x.shape = (45,1)
+    #        |pg|    pd and pg are variables, not fixed Pd and Pg
+    # 
+    #    g = |-U|      so g.shape = (45,1)
+    #        | C| 
+    #
+    #    A = | e|    with A.shape = (45,1)
+    #        |-e|    Where "e" is a vector of 1s with shape(Pd)
+    #                and "-e" is a vector of -1s with shape(Pg)
+    #
+    #    b = 0         so b.shape = ( 1,1)
+    #
+    #    l = | 0|    with l.shape = (45,1)
+    #
+    #    u = |Pd|      so u.shape = (45,1)
+    #        |Pg|    here Pd and Pg are the imported arrays
+    #
+    #==============================================================
+    # Build the matrices into the form
+    g = np.vstack((-U,C))
+    A = np.vstack((np.ones_like(Pd_max),-np.ones_like(Pg_max)))
+    b = np.array([[0]])
+    l = np.zeros_like(g)
+    u = np.vstack((Pd_max,Pg_max))
+    # Check Shapes
+    if lu_print == 1:
+        print("        Shapes:")
+        print("     g:",g.shape)
+        print("     A:",A.shape)
+        print("     b:",b.shape)
+        print("     l:",l.shape)
+        print("     u:",u.shape)
+    #==============================================================
+    # Next we want to convert the problem to fit into the solver described in lecture 07B
+    #
+    #         o --------------------- o
+    #         |   min   g.T @ x       |
+    #         |    x                  |
+    #         |   s.t.  A.T @ x  = b  | 
+    #         |               x >= 0  |
+    #         o --------------------- o
+    #
+    # Because of the structure of this problem, the lower bound is actually already 0, so we have:
+    #
+    #               0 <= x <= u
+    #
+    # We can then define:
+    #
+    #              z = |x-0| = | x |     
+    #                  |u-x|   |u-x|                                  
+    #
+    # We want to maintain A.T @ x = b, so with z we have:
+    #
+    #                    |A|   @ z = |A.T O.T| @ z = b        with O.shape = A.shape
+    #                    |O|.T                             where O is a matrix of 0s
+    #
+    # If we say that the vector x has N elements, we have then that z has 2N elements.
+    # For each n in range(N), we then have:
+    #
+    #                    z_{n} + z_{n+N} = x_{n} + u_{n} - x_{n} = u_{n}
+    #
+    # These are then N new equality constraints. We add these to our constraints matrix:
+    #
+    #       A_bar.T = |A.T       O.T       | so we have: A_bar.T @ z = |b|
+    #                 |1 0 0 ... 1 0 0 ... |                           |u|  
+    #                 |0 1 0 ... 0 1 0 ... |      
+    #                 | ...       ...      |       This means: A_bar = |A I|
+    #                 | ... 0 1 0 ... 0 1 0|                           |O I|
+    #                 | ... 0 0 1 ... 0 0 1|       With I.shape = (len(u),len(u)) = (n,n)   
+    #
+    # Since we have changed variables, we then also have to change our g. We want to minimize g.T @ x. 
+    # Luckily for us, z contains all of x as elements. This means we can just have:
+    #
+    #         g_bar = |g| so we have: g_bar.T @ z = g.T @ x    with o.shape = (u-x).shape = x.shape
+    #                 |o|                                                 where o is a vector of 0s
+    # 
+    # This then gives us:
+    #
+    #         o ------------------------- o
+    #         |   min   g_bar.T @ z       |
+    #         |    z                  |b| |
+    #         |   s.t.  A_bar.T @ z = |u| |
+    #         |                  z >= 0   |
+    #         o ------------------------- o 
+    #
+    #==============================================================
+    #Setting up our variables: 
+    un, _ = u.shape
+    z     = np.vstack((  np.ones_like(g),
+                    u-np.ones_like(g))) #note here that g.shape = x.shape
+    A_bar = np.block([[       A        ,np.eye(un)],  #A_bar = |A I| A_bar.T = |A.T O.T|
+                    [np.zeros_like(A),np.eye(un)]]) #        |O I|           | I   I |
+    b_bar = np.vstack((b,
+                    u))
+    g_bar = np.vstack((g,
+                    np.zeros_like(g)))
+    #Checking shapes:
+    if standard_print == 1:
+        print("        Shapes:")
+        print(" g_bar:",g_bar.shape)
+        print(" A_bar:",A_bar.shape)
+        print(" b_bar:",b_bar.shape)
+        print("     z:",z.shape)
 
 #==============================================================
 # Part 2: Solver
@@ -278,69 +279,70 @@ def LPippd(g,A,b,x="Empty"):
 #==============================================================
 # Part 3: Using the Solver and Statistics
 #==============================================================
-# Solution Baseline From 3.3:
-# 3.3 Used scipy.optimize.linprog, an example of how this can be done is below.
-bounds = []
-ln, _ = l.shape
-for i in range(ln):
-    bounds.append((float(l.flatten()[i]), float(u.flatten()[i])))
-sol = sp.optimize.linprog(c=g.flatten(),A_eq=A.T,b_eq=b.flatten(),bounds=bounds,method='highs')
-sp_x = np.array(sol.x).T 
-# This produces the following solution:
-x_33 = np.array([[225.0], [234.0], [213.0], [228.0], [231.0],
-                 [  0.0], [218.0], [  0.0], [  0.0], [225.0],
-                 [209.0], [  0.0], [222.0], [242.0], [  0.0],
-                 [245.0], [  0.0], [217.0], [  0.0], [242.0],
-                 [243.0], [230.0], [204.0], [230.0], [  0.0],
-                 [  0.0], [245.0], [  0.0], [230.0], [203.0],
-                 [406.0], [  0.0], [560.0], [310.0], [460.0],
-                 [ 45.0], [165.0], [  0.0], [420.0], [  0.0],
-                 [785.0], [  0.0], [  0.0], [410.0], [975.0]])
-#We then confirm that scipy solution matches the one we use in 3.3
-#print("Euclidean Distance Between Official 3.3 Solution and Recalculated SciPy Soltuion",np.linalg.norm(sp_x.flatten()-x_33.flatten(),ord=2))
-#Printing the real solution and cost
-print("Real Solution:\n", x_33.T) # Solution is 2D
-real_cost = (g.T @ x_33)[0,0]     # Use [0,0] to extract the cost value
-print("3.3 Cost:",real_cost)
+if __name__ == "__main__":
+    # Solution Baseline From 3.3:
+    # 3.3 Used scipy.optimize.linprog, an example of how this can be done is below.
+    bounds = []
+    ln, _ = l.shape
+    for i in range(ln):
+        bounds.append((float(l.flatten()[i]), float(u.flatten()[i])))
+    sol = sp.optimize.linprog(c=g.flatten(),A_eq=A.T,b_eq=b.flatten(),bounds=bounds,method='highs')
+    sp_x = np.array(sol.x).T 
+    # This produces the following solution:
+    x_33 = np.array([[225.0], [234.0], [213.0], [228.0], [231.0],
+                    [  0.0], [218.0], [  0.0], [  0.0], [225.0],
+                    [209.0], [  0.0], [222.0], [242.0], [  0.0],
+                    [245.0], [  0.0], [217.0], [  0.0], [242.0],
+                    [243.0], [230.0], [204.0], [230.0], [  0.0],
+                    [  0.0], [245.0], [  0.0], [230.0], [203.0],
+                    [406.0], [  0.0], [560.0], [310.0], [460.0],
+                    [ 45.0], [165.0], [  0.0], [420.0], [  0.0],
+                    [785.0], [  0.0], [  0.0], [410.0], [975.0]])
+    #We then confirm that scipy solution matches the one we use in 3.3
+    #print("Euclidean Distance Between Official 3.3 Solution and Recalculated SciPy Soltuion",np.linalg.norm(sp_x.flatten()-x_33.flatten(),ord=2))
+    #Printing the real solution and cost
+    print("Real Solution:\n", x_33.T) # Solution is 2D
+    real_cost = (g.T @ x_33)[0,0]     # Use [0,0] to extract the cost value
+    print("3.3 Cost:",real_cost)
 
-#==============================================================
-# Our Solution:
-#Using our LPippd
-zout, mu, la, res, iter, cpu_time = LPippd(g_bar,A_bar,b_bar,z) 
-zn, _ = zout.shape
-#Prining our solution and cost
-xout = np.array(zout[0:int(zn/2)]) # remember that our x is the first 45 elements of z. This is a 2 dimensional array
-print("Our Solutiuon:\n", xout.T)  # This means that xout is 2D as well
-our_cost = (g.T @ xout)[0,0]       # We then need to use [0,0] to get our cost
-print("Our Cost:",our_cost)
+    #==============================================================
+    # Our Solution:
+    #Using our LPippd
+    zout, mu, la, res, iter, cpu_time = LPippd(g_bar,A_bar,b_bar,z) 
+    zn, _ = zout.shape
+    #Prining our solution and cost
+    xout = np.array(zout[0:int(zn/2)]) # remember that our x is the first 45 elements of z. This is a 2 dimensional array
+    print("Our Solutiuon:\n", xout.T)  # This means that xout is 2D as well
+    our_cost = (g.T @ xout)[0,0]       # We then need to use [0,0] to get our cost
+    print("Our Cost:",our_cost)
 
-#==============================================================
-# Comparison:
-print("Solution Distance:", np.linalg.norm((x_33).flatten()-xout.flatten(),ord=2)) # We found a different minima
-print("Cost Difference:",real_cost - our_cost)                                      # But the cost is almost identical, off by an order of e-8
-#==============================================================
-# Plotting residuals:
-# Here we plot the residuals as a funciton of iteration:
-res = np.array(res)
-res_names = ["Lagrangian Gradient", "Equality Constaint", "Complementarity", "Duality Gap"]
-plt.figure(figsize=(10, 6))
+    #==============================================================
+    # Comparison:
+    print("Solution Distance:", np.linalg.norm((x_33).flatten()-xout.flatten(),ord=2)) # We found a different minima
+    print("Cost Difference:",real_cost - our_cost)                                      # But the cost is almost identical, off by an order of e-8
+    #==============================================================
+    # Plotting residuals:
+    # Here we plot the residuals as a funciton of iteration:
+    res = np.array(res)
+    res_names = ["Lagrangian Gradient", "Equality Constaint", "Complementarity", "Duality Gap"]
+    plt.figure(figsize=(10, 6))
 
-for i in range(res.shape[1]):
-    plt.plot(res[:, i], label=res_names[i])
+    for i in range(res.shape[1]):
+        plt.plot(res[:, i], label=res_names[i])
 
-plt.xlabel('Iteration Number')
-plt.ylabel('Residual L2 Norm')
-plt.title('LPippd: L2 Norm of Residuals By Iteration')
-plt.yscale('log') 
-plt.legend()
-plt.grid(True)
-plt.tight_layout()
-#plt.savefig("figures/3_5_Residuals.png")
-plt.show()
+    plt.xlabel('Iteration Number')
+    plt.ylabel('Residual L2 Norm')
+    plt.title('LPippd: L2 Norm of Residuals By Iteration')
+    plt.yscale('log') 
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+    #plt.savefig("figures/3_5_Residuals.png")
+    plt.show()
 
-print("Number of Iterations:", iter)
-print("CPU Time in seconds:", cpu_time / 1000000000)
+    print("Number of Iterations:", iter)
+    print("CPU Time in seconds:", cpu_time / 1000000000)
 
-#==============================================================
-# End Problem 3.5
-#==============================================================
+    #==============================================================
+    # End Problem 3.5
+    #==============================================================
--- prob3_8.py
+++ prob3_8.py
@@ -5,7 +5,170 @@ import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
 import time
-from prob3_5 import *
-
+import random
+from prob3_5 import LPippd
+# Print commands
+print_single  = 0
+print_n10     = 1
+print_n100    = 1
+print_n1000   = 1
 #==============================================================
 # 3.8.1: Using LPippd on random problems of different sizes
+#==============================================================
+# First we make a function to produce random problems of the form LPippd accepts:
+#
+#       o--------------------o
+#       | min   g.T @ x      |  
+#       |  x                 |  
+#       | s.t.  A.T @ x  = b | 
+#       |             x >= 0 | 
+#       o--------------------o
+#
+#==============================================================
+# The following function makes random feasible LP systems by building them from the KKT conditions.
+# The function itself is based on the one we used for 1.4. It accepts:
+#
+#       n: the number of x variables
+#    beta: a parameter which gives m by m = beta * n
+# density: a parameter giving the proportion of A which is nonzero
+#
+# The first thing we do is build A of shape (n,m) such that A.T has full row rank. This means we do not have repeat equality constraints.
+#
+# The next thing we do is define x0 to be a random position vector (x >= 0)
+# We do the same with the Lagrange multipliers for the equality constraint y0, and for the bound constraints s0 (dual feasibility)
+#
+# We then define b = A.T @ x0 (primal feasibility) and g = A @ y0 + s0 (stationarity)
+#
+# This almost always generates a feasible LP system.
+def random_lp_strict(n, beta=0.5,density=0.15): 
+        m = int(np.round(beta * n))
+        A = sp.sparse.random(n, m, density=density, format='csr', data_rvs=np.random.randn).toarray()
+
+        it_count=0
+        while np.linalg.matrix_rank(A.T) != A.T.shape[0]:
+            A = sp.sparse.random(n, m, density=density, format='csr', data_rvs=np.random.randn).toarray()
+            it_count+=1
+            if it_count>10000: raise ValueError("###---Too Many Iterations---###")
+
+        x0 = np.abs(np.random.randn(n,1)) # x >= 0
+        y0 = np.abs(np.random.randn(m,1)) 
+        s0 = np.abs(np.random.randn(n,1)) # dual feasibility
+
+        b = A.T @ x0                      # primal feasibility
+        g = A@y0 + s0                     # stationarity
+        
+        return g, A, b, x0, y0, s0
+#==============================================================
+# First we test our solver on a single generated problem
+g, A, b, x0, y0, s0 = random_lp_strict(5,0.5,0.15)
+x, mu, la, ResidualsArray, it_count, cpu_time = LPippd(g,A,b)
+sol = sp.optimize.linprog(c=g.flatten(),A_eq=A.T,b_eq=b.flatten(),method='highs')
+sp_x = np.array(sol.x) 
+if print_single == 1:
+    print("Real x:",sp_x.flatten())
+    print("Our x:",x.flatten())
+    print("Euclidean Distance Between Solutions:",np.linalg.norm(x.flatten()-sp_x.flatten(),ord=2))
+    print("Real Cost:", (g.T@sp_x)[0])
+    print("Our Cost:", (g.T@x)[0,0])
+
+#==============================================================
+# Here we test on many test problems with 10 x variables
+# This sometimes errors for reasons I am not certain about.
+# This behavior occasionally happens even after setting the random seed.
+# If it errors, just run it again and it should work.
+random.seed(1)
+if print_n10 == 1:
+    num=1000
+    size = 10
+    # Initialize vectors
+    EucDistVec = np.ones(num)
+    CostDifVec = np.ones(num)
+    ItVec      = np.ones(num)
+    CPUTimeVec = np.ones(num)
+    # For loop
+    total_t = time.process_time_ns()
+    for i in range(num):
+        g, A, b, x0, y0, s0 = random_lp_strict(size,0.5,0.15)
+        sol = sp.optimize.linprog(c=g.flatten(),A_eq=A.T,b_eq=b.flatten(),method='highs')
+        sp_x = np.array(sol.x).T 
+        x, mu, la, ResidualsArray, it_count, cpu_time = LPippd(g,A,b)
+        EucDistVec[i] = np.linalg.norm(x.flatten()-sp_x.flatten(),ord=2)
+        CostDifVec[i] = np.abs((g.T@sp_x)[0]-(g.T@x)[0,0])
+        ItVec[i]      = it_count
+        CPUTimeVec[i] = cpu_time
+    total_t = time.process_time_ns() - total_t  
+
+    print("\n---------- Testing on",len(EucDistVec),"Problems of Size n =",size,"----------")
+    print("Mean Euclidean Distance:",np.mean(EucDistVec))
+    print("Mean Cost Difference:", np.mean(CostDifVec))
+    print("Mean Number of LPippd Iterations:",np.mean(ItVec))
+    print("Mean LPippd CPU Time (Seconds):",np.mean(CPUTimeVec)/1000000000)
+    print("Total CPU Time (Seconds):",total_t/1000000000)
+#==============================================================
+# Here we test on many test problems with 100 x variables
+# This sometimes errors for reasons I am not certain about.
+# This behavior occasionally happens even after setting the random seed.
+# If it errors, just run it again and it should work.
+random.seed(1)
+if print_n100 == 1:
+    num=500
+    size = 100
+    # Initialize vectors
+    EucDistVec = np.ones(num)
+    CostDifVec = np.ones(num)
+    ItVec      = np.ones(num)
+    CPUTimeVec = np.ones(num)
+    # For loop
+    total_t = time.process_time_ns()
+    for i in range(num):
+        g, A, b, x0, y0, s0 = random_lp_strict(size,0.5,0.15)
+        sol = sp.optimize.linprog(c=g.flatten(),A_eq=A.T,b_eq=b.flatten(),method='highs')
+        sp_x = np.array(sol.x).T 
+        x, mu, la, ResidualsArray, it_count, cpu_time = LPippd(g,A,b)
+        EucDistVec[i] = np.linalg.norm(x.flatten()-sp_x.flatten(),ord=2)
+        CostDifVec[i] = np.abs((g.T@sp_x)[0]-(g.T@x)[0,0])
+        ItVec[i]      = it_count
+        CPUTimeVec[i] = cpu_time
+    total_t = time.process_time_ns() - total_t  
+
+    print("\n---------- Testing on",len(EucDistVec),"Problems of Size n =",size,"----------")
+    print("Mean Euclidean Distance:",np.mean(EucDistVec))
+    print("Mean Cost Difference:", np.mean(CostDifVec))
+    print("Mean Number of LPippd Iterations:",np.mean(ItVec))
+    print("Mean LPippd CPU Time (Seconds):",np.mean(CPUTimeVec)/1000000000)
+    print("Total CPU Time (Seconds):",total_t/1000000000)
+
+#==============================================================
+# Here we test on a couple test problems with 1000 x variables
+# This sometimes errors for reasons I am not certain about.
+# This behavior occasionally happens even after setting the random seed.
+# If it errors, just run it again and it should work.
+random.seed(1)
+if print_n1000 == 1:
+    num=5
+    size = 1000
+    # Initialize vectors
+    EucDistVec = np.ones(num)
+    CostDifVec = np.ones(num)
+    ItVec      = np.ones(num)
+    CPUTimeVec = np.ones(num)
+    # For loop
+    total_t = time.process_time_ns()
+    for i in range(num):
+        g, A, b, x0, y0, s0 = random_lp_strict(size,0.5,0.15)
+        sol = sp.optimize.linprog(c=g.flatten(),A_eq=A.T,b_eq=b.flatten(),method='highs')
+        sp_x = np.array(sol.x).T 
+        x, mu, la, ResidualsArray, it_count, cpu_time = LPippd(g,A,b)
+        EucDistVec[i] = np.linalg.norm(x.flatten()-sp_x.flatten(),ord=2)
+        CostDifVec[i] = np.abs((g.T@sp_x)[0]-(g.T@x)[0,0])
+        ItVec[i]      = it_count
+        CPUTimeVec[i] = cpu_time
+    total_t = time.process_time_ns() - total_t  
+
+    print("\n---------- Testing on",len(EucDistVec),"Problems of Size n =",size,"----------")
+    print("Mean Euclidean Distance:",np.mean(EucDistVec))
+    print("Mean Cost Difference:", np.mean(CostDifVec))
+    print("Mean Number of LPippd Iterations:",np.mean(ItVec))
+    print("Mean LPippd CPU Time (Seconds):",np.mean(CPUTimeVec)/1000000000)
+    print("Total CPU Time (Seconds):",total_t/1000000000)
+
